c7ea363c6a3a30413556e6ce4427f5778e4ddfac
Martina <martina.malberti@cern.ch>
2015-10-13 14:38:35 +0200

    small updates

--- MicroAOD/python/flashggMicroAODPhotonJetValidationSequence_cff.py
+++ MicroAOD/python/flashggMicroAODPhotonJetValidationSequence_cff.py
@@ -4,12 +4,21 @@ from flashgg.MicroAOD.flashggPhotons_cfi import flashggPhotons
 from flashgg.MicroAOD.flashggPhotonJet_cfi import flashggPhotonJet
 from flashgg.MicroAOD.flashggMicroAODGenSequence_cff import *
 
-flashggMicroAODPhotonJetValidationSequence = cms.Sequence(#eventCount+weightsCount
-    #                                   +
-    flashggVertexMapUnique+flashggVertexMapNonUnique
-    +flashggMicroAODGenSequence
-                                       +flashggPhotons
-                                       #+flashggFinalEGamma
-                                       +flashggPhotonJet
-                                       )
+eventCount = cms.EDProducer("EventCountProducer")
+weightsCount = cms.EDProducer("WeightsCountProducer",
+                              generator=cms.InputTag("generator"),
+                              pileupInfo=cms.InputTag("addPileupInfo"),
+                              doObsPileup=cms.untracked.bool(True),
+                              minObsPileup=cms.double(-0.5),
+                              maxObsPileup=cms.double(100.5),
+                              nbinsObsPileup=cms.int32(101),
+                              )
+
+flashggMicroAODPhotonJetValidationSequence = cms.Sequence(eventCount+weightsCount
+                                                          +flashggVertexMapUnique+flashggVertexMapNonUnique
+                                                          +flashggMicroAODGenSequence
+                                                          +flashggPhotons
+                                                          #+flashggFinalEGamma
+                                                          +flashggPhotonJet
+                                                          )
 
--- MicroAOD/test/microAODForPhotonJetValidation.py
+++ MicroAOD/test/microAODForPhotonJetValidation.py
@@ -3,6 +3,10 @@ import FWCore.Utilities.FileUtils as FileUtils
 
 process = cms.Process("FLASHggMicroAOD")
 
+process.load('HLTrigger.HLTfilters.hltHighLevel_cfi')
+process.hltHighLevel.throw = cms.bool(False)
+process.hltHighLevel.HLTPaths = ["HLT_Photon50_v*"]
+
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.load("Configuration.StandardSequences.GeometryDB_cff")
@@ -13,7 +17,7 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1000 ) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( -1) )
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 10 )
 
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
@@ -26,23 +30,12 @@ if current_gt.count("::All"):
     process.GlobalTag.globaltag = new_gt
 
 # Spring15
-#process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/mc/RunIISpring15DR74/ttHJetToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/70000/0232BC3C-01FF-E411-8779-0025907B4FC2.root"))
 process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring(
-        "/store/mc/RunIISpring15DR74/GJet_Pt-15To6000_TuneCUETP8M1-Flat_13TeV_pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v3/00000/00BB8CCC-9208-E511-8A90-0025905B857C.root"
-        #"/store/mc/RunIISpring15DR74/GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCUETP8M1_13TeV_Pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v2/70000/9038945A-6E0C-E511-91E9-02163E010FE8.root"
-        #"/store/mc/RunIISpring15DR74/GluGluHToGG_M-125_13TeV_powheg_pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/30000/54ECB9A4-912E-E511-BB7D-002590A831CA.root"
+        "/store/data/Run2015D/SinglePhoton/MINIAOD/05Oct2015-v1/10000/C8770AB6-B76F-E511-8D38-0026189437F0.root"
+        #"/store/mc/RunIISpring15DR74/GJet_Pt-15To6000_TuneCUETP8M1-Flat_13TeV_pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v3/00000/00BB8CCC-9208-E511-8A90-0025905B857C.root"
         ))
 process.MessageLogger.cerr.threshold = 'ERROR' # can't get suppressWarning to work: disable all warnings for now
-# process.MessageLogger.suppressWarning.extend(['SimpleMemoryCheck','MemoryCheck']) # this would have been better...
 
-# Uncomment the following if you notice you have a memory leak
-# This is a lightweight tool to digg further
-#process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
-#                                        ignoreTotal = cms.untracked.int32(1),
-#                                        monitorPssAndPrivate = cms.untracked.bool(True)
-#                                       )
-
-#process.load("flashgg/MicroAOD/flashggPDFWeightObject_cfi")
 process.load("flashgg/MicroAOD/flashggMicroAODPhotonJetValidationSequence_cff")
 
 from flashgg.MicroAOD.flashggMicroAODPhotonJetValidationOutputCommands_cff import microAODPhotonJetValidationOutputCommand
@@ -50,24 +43,8 @@ process.out = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.stri
                                outputCommands = microAODPhotonJetValidationOutputCommand
                                )
 
-# All jets are now handled in MicroAODCustomize.py
-# Switch from PFCHS to PUPPI with puppi=1 argument (both if puppi=2)
-
-process.p = cms.Path(process.flashggMicroAODPhotonJetValidationSequence)
-#process.p = cms.Path(process.flashggPDFWeightObject*process.flashggMicroAODSequence)
+process.p = cms.Path(process.hltHighLevel+process.flashggMicroAODPhotonJetValidationSequence)
 process.e = cms.EndPath(process.out)
-
-# Uncomment these lines to run the example commissioning module and send its output to root
-#process.commissioning = cms.EDAnalyzer('flashggCommissioning',
-#                                       PhotonTag=cms.untracked.InputTag('flashggPhotons'),
-#                                       DiPhotonTag = cms.untracked.InputTag('flashggDiPhotons'),
-#                                       VertexTag=cms.untracked.InputTag('offlineSlimmedPrimaryVertices')
-#)
-#process.TFileService = cms.Service("TFileService",
-#                                   fileName = cms.string("commissioningTree.root")
-#)
-#process.p *= process.commissioning
-
 
 #from flashgg.MicroAOD.MicroAODCustomize import customize
 #customize(process)
