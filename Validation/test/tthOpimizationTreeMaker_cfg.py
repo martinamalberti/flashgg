d02ceb98ce2e2a7d9fb585305ca844f8c02b2d14
Martina <martina.malberti@cern.ch>
2015-11-10 10:18:39 +0100

    added jet flavour

--- Validation/test/tthOpimizationTreeMaker_cfg.py
+++ Validation/test/tthOpimizationTreeMaker_cfg.py
@@ -10,7 +10,7 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = 'POSTLS170_V5::All'
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 100 ) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1000) )
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
 
 #process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/mc/Spring14miniaod/GluGluToHToGG_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_POSTLS170_V5-v2/00000/24926621-F11C-E411-AB9A-02163E008D0B.root"))
@@ -18,6 +18,8 @@ process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
 process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring(
         "/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISpring15-ReMiniAOD-BetaV7-25ns/Spring15BetaV7/ttHJetToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15-ReMiniAOD-BetaV7-25ns-Spring15BetaV7-v0-RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151021_153138/0000/myMicroAODOutputFile_2.root"
         #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISpring15-ReMiniAOD-BetaV7-25ns/Spring15BetaV7/GluGluHToGG_M-125_13TeV_powheg_pythia8/RunIISpring15-ReMiniAOD-BetaV7-25ns-Spring15BetaV7-v0-RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151021_152108/0000/myMicroAODOutputFile_1.root"
+        #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISpring15-ReMiniAOD-BetaV7-25ns/Spring15BetaV7/DoubleEG/RunIISpring15-ReMiniAOD-BetaV7-25ns-Spring15BetaV7-v0-Run2015D-05Oct2015-v1/151021_151712/0000/myMicroAODOutputFile_1.root"
+        #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISpring15-ReMiniAOD-BetaV7-25ns/Spring15BetaV7/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15-ReMiniAOD-BetaV7-25ns-Spring15BetaV7-v0-RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151021_151505/0000/myMicroAODOutputFile_1.root"
         ))
 
 process.load("flashgg/Taggers/flashggTagSequence_cfi")
@@ -26,10 +28,12 @@ from flashgg.Taggers.flashggTags_cff import UnpackedJetCollectionVInputTag
 process.analysisTree = cms.EDAnalyzer('FlashggtthOptimizationTreeMaker',
                                    lumiWeight=cms.untracked.double(1000.),
                                    generatorInfo = cms.InputTag('generator'),  
+                                   genParticleTag = cms.InputTag( "flashggPrunedGenParticles" ),
                                    PileUpTag = cms.InputTag('slimmedAddPileupInfo'),
                                    rhoFixedGridCollection = cms.InputTag('fixedGridRhoAll'),
                                    VertexTag=cms.InputTag('offlineSlimmedPrimaryVertices'),
                                    DiPhotonTag = cms.InputTag('flashggDiPhotons'),
+                                   #DiPhotonTag = cms.InputTag('flashggPreselectedDiPhotons'),
                                    MVAResultTag=cms.InputTag('flashggDiPhotonMVA'),
                                    inputTagJets= UnpackedJetCollectionVInputTag,
                                    GenJetTag=cms.InputTag( "slimmedGenJets"),
@@ -37,6 +41,7 @@ process.analysisTree = cms.EDAnalyzer('FlashggtthOptimizationTreeMaker',
                                    MuonTag=cms.InputTag('flashggSelectedMuons'),
                                    jetPtThreshold = cms.untracked.double(20.),
                                    bTag = cms.untracked.string("pfCombinedInclusiveSecondaryVertexV2BJetTags"),
+                                   triggerBits = cms.InputTag('TriggerResults::HLT')
                                   )
 
 process.TFileService = cms.Service("TFileService",
