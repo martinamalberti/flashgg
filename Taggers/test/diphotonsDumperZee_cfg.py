35a123b39f1df3e4df3b699ef8634b080a7c65e2
Martina <martina.malberti@cern.ch>
2015-12-21 16:56:26 +0100

    updated

--- Taggers/test/diphotonsDumperZee_cfg.py
+++ Taggers/test/diphotonsDumperZee_cfg.py
@@ -12,14 +12,15 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condD
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag.globaltag = '74X_mcRun2_asymptotic_v4' # keep updated for JEC
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 10 )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
 
 
 # input files
 process.source = cms.Source("PoolSource",
                             fileNames=cms.untracked.vstring(
-        "/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISpring15-ReMiniAOD-BetaV7-25ns/Spring15BetaV7/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15-ReMiniAOD-BetaV7-25ns-Spring15BetaV7-v0-RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151021_151505/0000/myMicroAODOutputFile_10.root"
+        #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISpring15-ReMiniAOD-BetaV7-25ns/Spring15BetaV7/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15-ReMiniAOD-BetaV7-25ns-Spring15BetaV7-v0-RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151021_151505/0000/myMicroAODOutputFile_10.root",
         #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISpring15-FinalPrompt-BetaV7-25ns/Spring15BetaV7/DoubleEG/RunIISpring15-FinalPrompt-BetaV7-25ns-Spring15BetaV7-v0-Run2015D-PromptReco-v4/151124_234634/0000/myMicroAODOutputFile_1.root"
+        "/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISpring15-ReMiniAOD-SilverNotGold-BetaV7-25ns/Spring15BetaV7/SingleElectron/RunIISpring15-ReMiniAOD-SilverNotGold-BetaV7-25ns-Spring15BetaV7-v0-Run2015D-05Oct2015-v1/151106_142931/0000/myMicroAODOutputFile_72.root"
         )
                             )
 
@@ -32,9 +33,6 @@ process.TFileService = cms.Service("TFileService",
 
 process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
                                                    flashggDiPhotonSystematics = cms.PSet(initialSeed = cms.untracked.uint32(664)),
-                                                   flashggElectronSystematics = cms.PSet(initialSeed = cms.untracked.uint32(11)),
-                                                   flashggMuonSystematics = cms.PSet(initialSeed = cms.untracked.uint32(13)),
-                                                   flashggTagSystematics = cms.PSet(initialSeed = cms.untracked.uint32(999)),
                                                    flashggRandomizedPerPhotonDiPhotons = cms.PSet(initialSeed = cms.untracked.uint32(281765313))
                                                    )
 
@@ -49,6 +47,8 @@ from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
 
 # load randomizer for smearings
 process.load("flashgg.MicroAOD.flashggRandomizedPerPhotonDiPhotonProducer_cff")
+process.flashggRandomizedPerPhotonDiPhotons.src = cms.InputTag("flashggDiPhotons")
+
 
 # load syst producer
 process.load("flashgg.Systematics.flashggDiPhotonSystematics_cfi")
@@ -62,7 +62,7 @@ if customize.processType == 'data':
     for pset in process.flashggDiPhotonSystematics.SystMethods:
         if pset.Label.value().count("Scale"):
             pset.NoCentralShift = cms.bool(False) # Turn on central shift for data (it is off for MC)                                                                              
-            pset.NSigmas = cms.vint32() # Do not perform shift
+            pset.NSigmas = cms.vint32() # Do only central value, no syst shifts
             newvpset += [pset]
     process.flashggDiPhotonSystematics.SystMethods = newvpset
 else:
@@ -71,7 +71,7 @@ else:
     newvpset = cms.VPSet()
     for pset in process.flashggDiPhotonSystematics.SystMethods:
         if pset.Label.value().count("Smear"):
-            pset.NSigmas = cms.vint32() # Do not perform shift
+            pset.NSigmas = cms.vint32() # Do only central value, no syst. shifts
             newvpset += [pset]
     process.flashggDiPhotonSystematics.SystMethods = newvpset
     
@@ -111,38 +111,44 @@ cfgTools.addCategories(process.diphotonDumper,
                        ## if different variables wanted for different categories, can add categorie one by one with cfgTools.addCategory
                        variables=["mass               :=mass",
                                   "vtxProbMVA         :=vtxProbMVA",
-                                  "cosdphi            := cos(leadingPhoton.phi-subLeadingPhoton.phi)",
+                                  "cosdphi            :=cos(leadingPhoton.phi-subLeadingPhoton.phi)",
                                   
                                   "pho1_pt            :=leadingPhoton.pt",
                                   "pho1_eta           :=leadingPhoton.superCluster.eta",
-                                  "pho1_r9            :=leadingPhoton.r9",
                                   "pho1_energy        :=leadingPhoton.energy",
                                   "pho1_rawEnergy     :=leadingPhoton.superCluster.rawEnergy",
                                   "pho1_eTrue         := ?leadingPhoton().hasMatchedGenPhoton()?leadingPhoton().matchedGenPhoton().energy():0",
-                                  "pho1_sieie         :=leadingPhoton.full5x5_sigmaIetaIeta",
+                                  "pho1_r9            :=leadingPhoton.r9",
+                                  "pho1_full5x5_r9    :=leadingPhoton.full5x5_r9",
+                                  "pho1_sieie         :=leadingPhoton.sigmaIetaIeta",
+                                  "pho1_full5x5_sieie :=leadingPhoton.full5x5_sigmaIetaIeta",
                                   "pho1_covieip       :=leadingPhoton.sieip",
                                   "pho1_etawidth      :=leadingPhoton.superCluster.etaWidth",
                                   "pho1_phiwidth      :=leadingPhoton.superCluster.phiWidth",
                                   "pho1_s4ratio       :=leadingPhoton.s4",
                                   "pho1_phoIso        :=leadingPhoton.pfPhoIso03",
                                   "pho1_ChgIso        :=leadingPhoton.pfChgIsoWrtChosenVtx03",
+                                  "pho1_ChgIsoWorstVtx:=leadingPhoton.pfChgIsoWrtWorstVtx03",
                                   "pho1_esEffSigmaRR  :=leadingPhoton.esEffSigmaRR",
                                   "pho1_idmva         :=leadPhotonId",
                                   "pho1_sigmaEoE      :=leadingPhoton.sigEOverE",
                                   
                                   "pho2_pt            :=subLeadingPhoton.pt",
                                   "pho2_eta           :=subLeadingPhoton.superCluster.eta",
-                                  "pho2_r9            :=subLeadingPhoton.r9",
                                   "pho2_energy        :=subLeadingPhoton.energy",
                                   "pho2_rawEnergy     :=subLeadingPhoton.superCluster.rawEnergy",
                                   "pho2_eTrue         := ?subLeadingPhoton().hasMatchedGenPhoton()?subLeadingPhoton().matchedGenPhoton().energy():0",
-                                  "pho2_sieie         :=subLeadingPhoton.full5x5_sigmaIetaIeta",
+                                  "pho2_r9            :=subLeadingPhoton.r9",
+                                  "pho2_full5x5_r9    :=subLeadingPhoton.full5x5_r9",
+                                  "pho2_sieie         :=subLeadingPhoton.sigmaIetaIeta",
+                                  "pho2_full5x5_sieie :=subLeadingPhoton.full5x5_sigmaIetaIeta",
                                   "pho2_covieip       :=subLeadingPhoton.sieip",
                                   "pho2_etawidth      :=subLeadingPhoton.superCluster.etaWidth",
                                   "pho2_phiwidth      :=subLeadingPhoton.superCluster.phiWidth",
                                   "pho2_s4ratio       :=subLeadingPhoton.s4",
                                   "pho2_phoIso        :=subLeadingPhoton.pfPhoIso03",
                                   "pho2_ChgIso        :=subLeadingPhoton.pfChgIsoWrtChosenVtx03",
+                                  "pho2_ChgIsoWorstVtx:=subLeadingPhoton.pfChgIsoWrtWorstVtx03",
                                   "pho2_esEffSigmaRR  :=subLeadingPhoton.esEffSigmaRR",
                                   "pho2_idmva         :=subLeadPhotonId",
                                   "pho2_sigmaEoE      :=subLeadingPhoton.sigEOverE"
@@ -160,6 +166,6 @@ process.p = cms.Path(process.hltHighLevel*
 
 # set default options if needed
 customize.setDefault("maxEvents",-1)
-customize.setDefault("targetLumi",2.5e+3)
+customize.setDefault("targetLumi",2.62e+3)
 # call the customization
 customize(process)
