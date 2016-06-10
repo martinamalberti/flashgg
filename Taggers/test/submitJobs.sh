e85213adc71d40125e8446dc7022f8129e83c621
Martina <martina.malberti@cern.ch>
2016-06-10 11:20:23 +0200

    jobs configs

---
+++ Taggers/test/submitJobs.sh
@@ -0,0 +1,7 @@
+fggRunJobs.py --load jobs_SingleElectron.json -H -D -P -n 10 -d jobsDiphotonDumper_data -x cmsRun diphotonsDumperZee_cfg.py maxEvents=-1 --no-use-tarball -q 8nh
+fggRunJobs.py --load jobs_DY.json -H -D -P -n 50 -d jobsDiphotonDumper_mc -x cmsRun diphotonsDumperZee_cfg.py maxEvents=-1 --no-use-tarball -q 8nh
+
+
+fggRunJobs.py --load jobsZee_DoubleEG.json -H -D -P -n 10 -d jobsZeeDumper_DoubleEG -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh
+fggRunJobs.py --load jobsZee_SingleElectron.json -H -D -P -n 100 -d jobsZeeDumper_SingleElectron_v2 -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh
+fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 100 -d jobsZeeDumper_MC_v2 -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh
