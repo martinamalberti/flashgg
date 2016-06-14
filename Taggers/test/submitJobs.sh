c55fcef6366021da840ba196febb8b8eed5a9c1d
Martina <martina.malberti@cern.ch>
2016-06-14 11:49:59 +0200

    update to submit jobs

--- Taggers/test/submitJobs.sh
+++ Taggers/test/submitJobs.sh
@@ -5,3 +5,8 @@ fggRunJobs.py --load jobs_DY.json -H -D -P -n 50 -d jobsDiphotonDumper_mc -x cms
 fggRunJobs.py --load jobsZee_DoubleEG.json -H -D -P -n 10 -d jobsZeeDumper_DoubleEG -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh
 fggRunJobs.py --load jobsZee_SingleElectron.json -H -D -P -n 100 -d jobsZeeDumper_SingleElectron_v2 -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh
 fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 100 -d jobsZeeDumper_MC_v2 -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh
+
+
+# to save output on eos
+mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/zee_validation/jobsZeeDumper_MC_DYToEE/
+fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 200 -d jobsZeeDumper_MC_DYToEE -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q cmscaf1nd --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/zee_validation/jobsZeeDumper_MC_DYToEE
