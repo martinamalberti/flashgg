1f8f5d60ea24691a815e2eb057d1664097003a2c
Martina <martina.malberti@cern.ch>
2016-06-27 10:31:15 +0200

    fixed catalogue name

--- Taggers/test/submitJobs.sh
+++ Taggers/test/submitJobs.sh
@@ -8,5 +8,5 @@ fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 100 -d jobsZeeDumper_MC_v2 -x c
 
 
 # to save output on eos
-mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/zee_validation/jobsZeeDumper_MC_DYToEE/
-fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 200 -d jobsZeeDumper_MC_DYToEE -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q cmscaf1nd --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/zee_validation/jobsZeeDumper_MC_DYToEE
+mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/zee_validation/jobsZeeDumper_MC_DYToEE_v4
+fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 200 -d jobsZeeDumper_MC_DYToEE_v4 -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q cmscaf1nd --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/zee_validation/jobsZeeDumper_MC_DYToEE_v4
