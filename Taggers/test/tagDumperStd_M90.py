23edd61e00503336169c4c0c945bab099c2d89af
Martina <martina.malberti@cern.ch>
2016-07-19 09:08:22 +0200

    updates to run on samples with material variations

---
+++ Taggers/test/tagDumperStd_M90.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env cmsRun
+
+import FWCore.ParameterSet.Config as cms
+import FWCore.Utilities.FileUtils as FileUtils
+from flashgg.Systematics.SystematicDumperDefaultVariables import minimalVariables,minimalHistograms,minimalNonSignalVariables,systematicVariables
+import os
+
+# SYSTEMATICS SECTION
+dropVBFInNonGold = False
+
+process = cms.Process("FLASHggSyst")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+if os.environ["CMSSW_VERSION"].count("CMSSW_7_6"):
+    process.GlobalTag.globaltag = '76X_mcRun2_asymptotic_v12'
+elif os.environ["CMSSW_VERSION"].count("CMSSW_7_4"):
+    process.GlobalTag.globaltag = '74X_mcRun2_asymptotic_v4' 
+elif os.environ["CMSSW_VERSION"].count("CMSSW_8_0"):
+    process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_2016_miniAODv2'
+else:
+    raise Exception,"Could not find a sensible CMSSW_VERSION for default globaltag"
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(300) )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 100 )
+
+
+
+
+from flashgg.Systematics.SystematicsCustomize import *
+jetSystematicsInputTags = createStandardSystematicsProducers(process)
+process.flashggTagSorter.MassCutLower=cms.double(50.)
+modifyTagSequenceForSystematics(process,jetSystematicsInputTags)
+
+systlabels = [""]
+phosystlabels = []
+jetsystlabels = []
+elesystlabels = []
+musystlabels = []
+
+# import flashgg customization to check if we have signal or background
+from flashgg.MetaData.JobConfig import customize
+customize.parse()
+print "customize.processId:",customize.processId
+
+# Or use the official tool instead
+useEGMTools(process)
+
+# Apply corrections for signal events
+myvariables = [ "mass       := diPhoton().mass", 
+                "diphoMVA   := diPhotonMVA().result",    
+                "leadPt     := diPhoton().leadingPhoton.pt",
+                "subleadPt  := diPhoton().subLeadingPhoton.pt",
+                "leadE      := diPhoton().leadingPhoton.energy()",
+                "subleadE   := diPhoton().subLeadingPhoton.energy()",
+                "leadEta    := diPhoton().leadingPhoton.eta()",
+                "subleadEta := diPhoton().subLeadingPhoton.eta()",
+                "leadR9     := diPhoton().leadingPhoton.full5x5_r9()",
+                "subleadR9  := diPhoton().subLeadingPhoton.full5x5_r9()",
+                "leadEtrue   := ?diPhoton.leadingPhoton().hasMatchedGenPhoton()?diPhoton.leadingPhoton().matchedGenPhoton().energy():0",
+                "subleadEtrue:= ?diPhoton.subLeadingPhoton().hasMatchedGenPhoton()?diPhoton.subLeadingPhoton().matchedGenPhoton().energy():0"
+                ]
+                
+variablesToUse = myvariables
+if customize.processId == "Data":
+    customizeSystematicsForData(process)
+else:
+    customizeSystematicsForBackground(process) # only central corrections, no syst. shifts
+
+print "--- Systematics  with independent collections ---"
+print systlabels
+print "-------------------------------------------------"
+print "--- Variables to be dumped, including systematic weights ---"
+print variablesToUse
+print "------------------------------------------------------------"
+
+cloneTagSequenceForEachSystematic(process,systlabels,phosystlabels,jetsystlabels,jetSystematicsInputTags)
+
+###### Dumper section
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+from flashgg.MetaData.samples_utils import SamplesManager
+
+process.source = cms.Source ("PoolSource",
+                             fileNames = cms.untracked.vstring(
+"/store/group/phys_higgs/cmshgg/ferriff/flashgg/RunIISpring16DR80X-2_2_0-25ns_ICHEP16_MiniAODv2/2_2_0/GluGluHToGG_M90_13TeV_amcatnloFXFX_pythia8/RunIISpring16DR80X-2_2_0-25ns_ICHEP16_MiniAODv2-2_2_0-v0-RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/160707_145238/0000/myMicroAODOutputFile_1.root"
+
+))
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("test.root"))
+
+process.extraDumpers = cms.Sequence()
+process.load("flashgg.Taggers.diphotonTagDumper_cfi") ##  import diphotonTagDumper 
+import flashgg.Taggers.dumperConfigTools as cfgTools
+
+process.tagsDumper.className = "DiPhotonTagDumper"
+process.tagsDumper.src = "flashggSystTagMerger"
+#process.tagsDumper.src = "flashggTagSystematics"
+process.tagsDumper.processId = "test"
+process.tagsDumper.dumpTrees = True
+process.tagsDumper.dumpWorkspace = False
+process.tagsDumper.dumpHistos = False
+process.tagsDumper.quietRooFit = True
+process.tagsDumper.nameTemplate = cms.untracked.string("$PROCESS_$SQRTS_$CLASSNAME_$SUBCAT_$LABEL")
+
+tagList=[
+["UntaggedTag",4],
+["VBFTag",2],
+#["VHTightTag",0],
+#["VHLooseTag",0],
+#["VHEtTag",0],
+#["VHHadronicTag",0],
+["TTHHadronicTag",0],
+["TTHLeptonicTag",0]
+]
+
+definedSysts=set()
+process.tagsDumper.classifierCfg.remap=cms.untracked.VPSet()
+for tag in tagList: 
+  tagName=tag[0]
+  tagCats=tag[1]
+  # remap return value of class-based classifier
+  process.tagsDumper.classifierCfg.remap.append( cms.untracked.PSet( src=cms.untracked.string("flashgg%s"%tagName), dst=cms.untracked.string(tagName) ) )
+  for systlabel in systlabels:
+      if not systlabel in definedSysts:
+          # the cut corresponding to the systematics can be defined just once
+          cutstring = "hasSyst(\"%s\") "%(systlabel)
+          definedSysts.add(systlabel)
+      else:
+          cutstring = None
+      if systlabel == "":
+          currentVariables = variablesToUse
+      else:
+          currentVariables = systematicVariables
+      
+      isBinnedOnly = (systlabel !=  "")
+      print "No PDF weights"
+      dumpPdfWeights = False
+      nPdfWeights = -1
+      nAlphaSWeights = -1
+      nScaleWeights = -1
+      
+      cfgTools.addCategory(process.tagsDumper,
+                           systlabel,
+                           classname=tagName,
+                           cutbased=cutstring,
+                           subcats=tagCats, 
+                           variables=currentVariables,
+                           histograms=minimalHistograms,
+                           binnedOnly=isBinnedOnly,
+                           dumpPdfWeights=dumpPdfWeights,
+                           nPdfWeights=nPdfWeights,
+                           nAlphaSWeights=nAlphaSWeights,
+                           nScaleWeights=nScaleWeights
+                           )
+
+# Require standard diphoton trigger
+from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
+process.hltHighLevel= hltHighLevel.clone(HLTPaths = cms.vstring("HLT_Diphoton30_18_R9Id_OR_IsoCaloId_AND_HE_R9Id_Mass90_v*",
+                                                                #"HLT_Diphoton30PV_18PV_R9Id_AND_IsoCaloId_AND_HE_R9Id_DoublePixelVeto_Mass55_v1",
+                                                                #"HLT_Diphoton30EB_18EB_R9Id_OR_IsoCaloId_AND_HE_R9Id_DoublePixelVeto_Mass55_v1"
+                                                                ))
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+# ee bad supercluster filter on data
+process.load('RecoMET.METFilters.eeBadScFilter_cfi')
+process.eeBadScFilter.EERecHitSource = cms.InputTag("reducedEgamma","reducedEERecHits") # Saved MicroAOD Collection (data only)
+process.dataRequirements = cms.Sequence()
+if customize.processId == "Data":
+        process.dataRequirements += process.hltHighLevel
+        process.dataRequirements += process.eeBadScFilter
+
+# Split WH and ZH
+process.genFilter = cms.Sequence()
+if (customize.processId.count("wh") or customize.processId.count("zh")) and not customize.processId.count("wzh"):
+    process.load("flashgg/Systematics/VHFilter_cfi")
+    process.genFilter += process.VHFilter
+    process.VHFilter.chooseW = bool(customize.processId.count("wh"))
+    process.VHFilter.chooseZ = bool(customize.processId.count("zh"))
+
+# Split out prompt-fake or fake-fake
+process.finalFilter = cms.Sequence()
+if (customize.processId.count("qcd") or customize.processId.count("gjet")) and customize.processId.count("fake"):
+    process.load("flashgg/Systematics/PromptFakeFilter_cfi")
+    process.finalFilter += process.PromptFakeFilter
+    if (customize.processId.count("promptfake")):
+        process.PromptFakeFilter.doPromptFake = cms.bool(True)
+        process.PromptFakeFilter.doFakeFake =cms.bool(False)
+    elif (customize.processId.count("fakefake")):
+        process.PromptFakeFilter.doPromptFake =cms.bool(False)
+        process.PromptFakeFilter.doFakeFake =cms.bool(True)
+    else:
+        raise Exception,"Mis-configuration of python for prompt-fake filter"
+
+process.p = cms.Path(process.dataRequirements*
+                     process.genFilter*
+                     process.flashggUpdatedIdMVADiPhotons*
+                     process.flashggDiPhotonSystematics*
+                     process.flashggMuonSystematics*process.flashggElectronSystematics*
+                     (process.flashggUnpackedJets*process.jetSystematicsSequence)*
+                     (process.flashggTagSequence*process.systematicsTagSequences)*
+                     process.flashggSystTagMerger*
+                     process.finalFilter*
+                     process.tagsDumper)
+
+print "--- Dumping modules that take diphotons as input: ---"
+mns = process.p.moduleNames()
+for mn in mns:
+    module = getattr(process,mn)
+    if hasattr(module,"src") and type(module.src) == type(cms.InputTag("")) and module.src.value().count("DiPhoton"):
+        print str(module),module.src
+    elif hasattr(module,"DiPhotonTag"):
+        print str(module),module.DiPhotonTag
+print
+printSystematicInfo(process)
+
+#from Validation.Performance.TimeMemoryInfo import customise as TimeMemoryCustomize
+#TimeMemoryCustomize(process)
+#process.MessageLogger.cerr.threshold = 'WARNING'
+
+#process.load("IgTools.IgProf.IgProfTrigger")
+#process.igprof.reportEventInterval     = cms.untracked.int32(250)
+#process.igprof.reportToFileAtBeginJob  = cms.untracked.string("|gzip -c>igprof.begin-job.gz")
+#process.igprof.reportToFileAtEvent     = cms.untracked.string("|gzip -c>igprof.%I.%E.%L.%R.event.gz")
+#process.p += process.igprof
+
+################################
+## Dump merged tags to screen ##
+################################
+
+#process.load("flashgg/Taggers/flashggTagTester_cfi")
+#process.flashggTagTester.TagSorter = cms.InputTag("flashggSystTagMerger")
+#process.flashggTagTester.ExpectMultiples = cms.untracked.bool(True)
+#process.p += process.flashggTagTester
+
+##############
+## Dump EDM ##
+##############
+
+#process.out = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.string('CustomizeWillChangeThisAnyway.root'),
+#                               outputCommands = cms.untracked.vstring('keep *') # dump everything! small tests only!
+#                               )
+#process.e = cms.EndPath(process.out)
+
+############################
+## Dump the output Python ##
+############################
+#print process.dumpPython()
+#processDumpFile = open('processDump.py', 'w')
+#print >> processDumpFile, process.dumpPython()
+
+# set default options if needed
+customize.setDefault("maxEvents",100)
+customize.setDefault("targetLumi",1e+3)
+# call the customization
+customize(process)
