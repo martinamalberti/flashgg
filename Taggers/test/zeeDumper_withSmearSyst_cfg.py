d5b099cc491efd80f50601c9b0db81ffb0303113
Martina <martina.malberti@cern.ch>
2016-07-20 11:57:35 +0200

    add new data

--- Taggers/test/zeeDumper_withSmearSyst_cfg.py
+++ Taggers/test/zeeDumper_withSmearSyst_cfg.py
@@ -167,14 +167,17 @@ process.tagsDumper.globalVariables.addTriggerBits = cms.PSet(
         "HLT_Diphoton30_18_R9Id_OR_IsoCaloId_AND_HE_R9Id_DoublePixelSeedMatch_Mass70_v2",
         "HLT_Diphoton30_18_R9Id_OR_IsoCaloId_AND_HE_R9Id_DoublePixelSeedMatch_Mass70_v3",
         "HLT_Diphoton30_18_R9Id_OR_IsoCaloId_AND_HE_R9Id_DoublePixelSeedMatch_Mass70_v4",
+        "HLT_Diphoton30_18_R9Id_OR_IsoCaloId_AND_HE_R9Id_DoublePixelSeedMatch_Mass70_v5",
         "HLT_Ele27_WPTight_Gsf_v1",
         "HLT_Ele27_WPTight_Gsf_v2",
         "HLT_Ele27_WPTight_Gsf_v3",
         "HLT_Ele27_WPTight_Gsf_v4",
+        "HLT_Ele27_WPTight_Gsf_v5",
         "HLT_Ele35_WPLoose_Gsf_v1",
         "HLT_Ele35_WPLoose_Gsf_v2",
         "HLT_Ele35_WPLoose_Gsf_v3",
         "HLT_Ele35_WPLoose_Gsf_v4",
+        "HLT_Ele35_WPLoose_Gsf_v5"
         #"HLT_Ele27_WPLoose_Gsf_v",
         #"HLT_Ele27_eta2p1_WPLoose_Gsf_v",
         #"HLT_Ele22_eta2p1_WPLoose_Gsf_v"
