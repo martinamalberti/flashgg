a1a90d98281aba208a7e8a05ede0843286f1e0cd
Martina <martina.malberti@cern.ch>
2017-03-01 15:24:01 +0100

    added LT binned DY samples

--- Taggers/test/submitJobs.sh
+++ Taggers/test/submitJobs.sh
@@ -1,11 +1,10 @@
 #to save on eos
-mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170222/Data
-mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170222/MC
-mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170222/MC_noSmearings
+mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170227/Data
+mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170227/MC
+mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170227/MC_LTbinned_v2
 
-fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 10 -d jobs_Zee_20170222_MC -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170222/MC
+fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 10 -d jobs_Zee_20170227_MC -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170227/MC
 
-fggRunJobs.py --load jobsZee_SingleElectron_reMiniAOD.json -H -D -P -n 10 -d jobs_Zee_20170222_Data -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170222/Data
+fggRunJobs.py --load jobsZee_SingleElectron_reMiniAOD.json -H -D -P -n 10 -d jobs_Zee_20170227_Data -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170227/Data
 
-fggRunJobs.py --load jobsZee_MC.json -H -D -P -n 10 -d jobs_Zee_20170222_MC_noSmearings -x cmsRun zeeDumper_noSmearings_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170222/MC_noSmearings
-
+fggRunJobs.py --load jobsZee_MC_LTbinned.json -H -D -P -n 10 -d jobs_Zee_20170227_MC_LTbinned_v2 -x cmsRun zeeDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/zee/zee_20170227/MC_LTbinned_v2
--- Taggers/test/zeeDumper_cfg.py
+++ Taggers/test/zeeDumper_cfg.py
@@ -29,8 +29,10 @@ process.source = cms.Source("PoolSource",
                             fileNames=cms.untracked.vstring(      
         #data                    
         #"/store/group/phys_higgs/cmshgg/ferriff/flashgg/RunIISpring16DR80X-2_3_0-25ns_Moriond17_MiniAODv2/2_3_0/DoubleEG/RunIISpring16DR80X-2_3_0-25ns_Moriond17_MiniAODv2-2_3_0-v0-Run2016E-23Sep2016-v1/161114_163114/0000/myMicroAODOutputFile_817.root"
+        #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/ReMiniAOD-03Feb2017-2_5_1/2_5_1/SingleElectron/ReMiniAOD-03Feb2017-2_5_1-2_5_1-v0-Run2016D-03Feb2017-v1/170214_121515/0000/myMicroAODOutputFile_550.root"
         # mc
-        "/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISummer16-2_4_0-25ns_Moriond17/2_4_0/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-herwigpp_30M/RunIISummer16-2_4_0-25ns_Moriond17-2_4_0-v0-RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/161225_213425/0000/myMicroAODOutputFile_1.root"
+        #"/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISummer16-2_4_0-25ns_Moriond17/2_4_0/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-herwigpp_30M/RunIISummer16-2_4_0-25ns_Moriond17-2_4_0-v0-RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/161225_213425/0000/myMicroAODOutputFile_1.root"
+        "/store/group/phys_higgs/cmshgg/sethzenz/flashgg/RunIISummer16-2_4_1-25ns_Moriond17/2_4_1/DYJetsToEE_M-50_LTbinned_95To100_5f_LO_13TeV-madgraph_pythia8/RunIISummer16-2_4_1-25ns_Moriond17-2_4_1-v0-RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/170118_090503/0000/myMicroAODOutputFile_5.root"
         ))
 
 ## output file
@@ -203,8 +205,12 @@ cfgTools.addCategories(process.diphotonDumper,
                                   "ele1_full5x5_sigmaIetaIeta := diPhoton.leadingPhoton.full5x5_sigmaIetaIeta",
                                   "ele1_sigmaEoE      :=diPhoton.leadingPhoton.sigEOverE",
                                   "ele1_pfPhoIso03    :=diPhoton.leadingPhoton.pfPhoIso03",
-                                  "ele1_ESoRawE        :=diPhoton.leadingPhoton.superCluster.preshowerEnergy()/diPhoton.leadingPhoton.superCluster.rawEnergy()",
+                                  "ele1_chIsoRV       :=diPhoton.leadingView.pfChIso03WrtChosenVtx()",
+                                  "ele1_chIsoWV       :=diPhoton.leadingPhoton.pfChgIsoWrtWorstVtx03",
+                                  "ele1_ESoRawE       :=diPhoton.leadingPhoton.superCluster.preshowerEnergy()/diPhoton.leadingPhoton.superCluster.rawEnergy()",
                                   "ele1_idmva         :=diPhoton.leadPhotonId",
+                                  "ele1_kHasSwitchToGain6 := diPhoton().leadingPhoton.checkStatusFlag('kHasSwitchToGain6')",
+                                  "ele1_kHasSwitchToGain1 := diPhoton().leadingPhoton.checkStatusFlag('kHasSwitchToGain1')",
 
                                   "ele2_et            :=diPhoton.subLeadingPhoton.pt",
                                   "ele2_energy        :=diPhoton.subLeadingPhoton.energy",
@@ -222,9 +228,13 @@ cfgTools.addCategories(process.diphotonDumper,
                                   "ele2_full5x5_sigmaIetaIeta := diPhoton.subLeadingPhoton.full5x5_sigmaIetaIeta",
                                   "ele2_sigmaEoE      :=diPhoton.subLeadingPhoton.sigEOverE",
                                   "ele2_pfPhoIso03    :=diPhoton.subLeadingPhoton.pfPhoIso03",
+                                  #"ele2_chIsoRV       :=diPhoton.subLeadingPhoton.pfChIso03WrtChosenVtx()",
+                                  "ele2_chIsoRV       :=diPhoton.subLeadingView.pfChIso03WrtChosenVtx()",
+                                  "ele2_chIsoWV       :=diPhoton.subLeadingPhoton.pfChgIsoWrtWorstVtx03",
                                   "ele2_ESoRawE       :=diPhoton.subLeadingPhoton.superCluster.preshowerEnergy()/diPhoton.subLeadingPhoton.superCluster.rawEnergy()",
                                   "ele2_idmva         :=diPhoton.subLeadPhotonId",
-
+                                  "ele2_kHasSwitchToGain6 := diPhoton().subLeadingPhoton.checkStatusFlag('kHasSwitchToGain6')",
+                                  "ele2_kHasSwitchToGain1 := diPhoton().subLeadingPhoton.checkStatusFlag('kHasSwitchToGain1')"
                                   ],
                        histograms=[]
                        )
