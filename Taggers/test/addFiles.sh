ffd1c1a1ebe986743ae06fd6077211ced59f4775
Martina <martina.malberti@cern.ch>
2017-03-27 11:39:37 +0200

    split in more jobs

--- Taggers/test/addFiles.sh
+++ Taggers/test/addFiles.sh
@@ -1,23 +1,23 @@
 MYDIR=~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170324/
 
-hadd -f $MYDIR/Sig/output_ggh.root $MYDIR/Sig/GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8_*.root
-hadd -f $MYDIR/Sig/output_vbf.root $MYDIR/Sig/VBFHToGG_M125_13TeV_amcatnloFXFX_pythia8_*.root
-hadd -f $MYDIR/Sig/output_vh.root $MYDIR/Sig/VHToGG_M125_13TeV_amcatnloFXFX_pythia8_*.root
-hadd -f $MYDIR/Sig/output_tth.root $MYDIR/Sig/ttHToGG_M125_13TeV_amcatnloFXFX_pythia8_*.root
+#hadd -f $MYDIR/Sig/output_ggh.root $MYDIR/Sig/GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8_*.root
+#hadd -f $MYDIR/Sig/output_vbf.root $MYDIR/Sig/VBFHToGG_M125_13TeV_amcatnloFXFX_pythia8_*.root
+#hadd -f $MYDIR/Sig/output_vh.root $MYDIR/Sig/VHToGG_M125_13TeV_amcatnloFXFX_pythia8_*.root
+#hadd -f $MYDIR/Sig/output_tth.root $MYDIR/Sig/ttHToGG_M125_13TeV_amcatnloFXFX_pythia8_*.root
 
-hadd -f $MYDIR/Bkg/output_diphoton.root $MYDIR/Bkg/DiPhotonJetsBox_*.root
-hadd -f $MYDIR/Bkg/output_gjet.root $MYDIR/Bkg/GJet*.root
-hadd -f $MYDIR/Bkg/output_qcd.root $MYDIR/Bkg/QCD*.root
+#hadd -f $MYDIR/Bkg/output_diphoton.root $MYDIR/Bkg/DiPhotonJetsBox_*.root
+#hadd -f $MYDIR/Bkg/output_gjet.root $MYDIR/Bkg/GJet*.root
+#hadd -f $MYDIR/Bkg/output_qcd.root $MYDIR/Bkg/QCD*.root
 hadd -f $MYDIR/Bkg/output_DYJetsToLL.root $MYDIR/Bkg/DYJetsToLL*.root
-hadd -f $MYDIR/Bkg/output_ZGTo2LG.root $MYDIR/Bkg/ZGTo2LG*root
-hadd -f $MYDIR/Bkg/output_WGToLNuG.root $MYDIR/Bkg/WGToLNuG*.root
-hadd -f $MYDIR/Bkg/output_ZZTo2L2Q.root $MYDIR/Bkg/ZZTo2L2Q*.root
-hadd -f $MYDIR/Bkg/output_WZTo2L2Q.root $MYDIR/Bkg/WZTo2L2Q*.root
-hadd -f $MYDIR/Bkg/output_WW.root $MYDIR/Bkg/WW*.root
-hadd -f $MYDIR/Bkg/output_TTGG_0Jets.root $MYDIR/Bkg/TTGG_0Jets*.root
+#hadd -f $MYDIR/Bkg/output_ZGTo2LG.root $MYDIR/Bkg/ZGTo2LG*root
+#hadd -f $MYDIR/Bkg/output_WGToLNuG.root $MYDIR/Bkg/WGToLNuG*.root
+#hadd -f $MYDIR/Bkg/output_ZZTo2L2Q.root $MYDIR/Bkg/ZZTo2L2Q*.root
+#hadd -f $MYDIR/Bkg/output_WZTo2L2Q.root $MYDIR/Bkg/WZTo2L2Q*.root
+#hadd -f $MYDIR/Bkg/output_WW.root $MYDIR/Bkg/WW*.root
+#hadd -f $MYDIR/Bkg/output_TTGG_0Jets.root $MYDIR/Bkg/TTGG_0Jets*.root
 hadd -f $MYDIR/Bkg/output_TTGJets.root $MYDIR/Bkg/TTGJets*.root
-hadd -f $MYDIR/Bkg/output_TGJets.root $MYDIR/Bkg/TGJets*.root
-hadd -f $MYDIR/Bkg/output_TTJets.root $MYDIR/Bkg/TTJets*.root
+#hadd -f $MYDIR/Bkg/output_TGJets.root $MYDIR/Bkg/TGJets*.root
+#hadd -f $MYDIR/Bkg/output_TTJets.root $MYDIR/Bkg/TTJets*.root
 
 
-hadd -f $MYDIR/Data/output_data.root $MYDIR/Data/DoubleEG*.root
+#hadd -f $MYDIR/Data/output_data.root $MYDIR/Data/DoubleEG*.root
--- Taggers/test/submitJobs.sh
+++ Taggers/test/submitJobs.sh
@@ -1,12 +1,12 @@
 #to save on eos
-mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170324/Sig
-mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170324/Bkg
-mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170324/Data
+mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170327/Sig
+mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170327/Bkg
+mkdir ~/eos/cms/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170327/Data
 
-fggRunJobs.py --load jobs_mc_sig_moriond17.json -H -D -P -n 10 -d jobs_vhLepDumper_20170324_Sig -x cmsRun vhLeptonicTagsDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170324/Sig/
+fggRunJobs.py --load jobs_mc_sig_moriond17.json -H -D -P -n 10 -d jobs_vhLepDumper_20170327_Sig -x cmsRun vhLeptonicTagsDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170327/Sig/
 
-fggRunJobs.py --load jobs_mc_bkg_moriond17.json -H -D -P -n 10 -d jobs_vhLepDumper_20170324_Bkg -x cmsRun vhLeptonicTagsDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170324/Bkg/
+fggRunJobs.py --load jobs_mc_bkg_moriond17.json -H -D -P -n 10 -d jobs_vhLepDumper_20170327_Bkg -x cmsRun vhLeptonicTagsDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170327/Bkg/
 
-fggRunJobs.py --load jobs_data_moriond17.json -H -D -P -n 10 -d jobs_vhLepDumper_20170324_Data -x cmsRun vhLeptonicTagsDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170324/Data/
+fggRunJobs.py --load jobs_data_moriond17.json -H -D -P -n 10 -d jobs_vhLepDumper_20170327_Data -x cmsRun vhLeptonicTagsDumper_cfg.py maxEvents=-1 --no-use-tarball -q 8nh --stage-to=/store/group/phys_higgs/cmshgg/malberti/flashgg/ntuples/vh/vhLepDumper_20170327/Data/
 
 
